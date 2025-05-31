c735c02d1b041801589b858e87b96060b64d2763
harsh14thakre <harsh14thakre@gmail.com>
2025-05-31 16:41:49 +0530

    commit

---
+++ Quicksort.cpp
@@ -0,0 +1,22 @@
+////  QUICK SORTING 
+// It is mostly useful sorting algorithm 
+// It follow Divide and conquer approach 
+// It use pivot element 
+// It divide array in two part may be or not
+// It also use Recurssion 
+
+// LOGIC of Quick Sort 
+//choose first element as a pivot 
+// then after pivot element first element as P and last element is Q
+// if P > Pivot then
+//    stop
+// else
+//    P++
+// and
+// if Q < Pivot then 
+//    stop
+// else
+//    Q--
+
+
+// If p and q didnt crossed each other then they swap
--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,39 +1,80 @@
-// in all cases time complexity is order of square
+// // in all cases time complexity is order of square
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[]={3,14,2,5,7};
+//     int s=sizeof(arr)/sizeof(arr[0]);
+
+//     for(int i=0;i<s;i++)
+//     {
+//         cout<<arr[i]<<"\t";
+//     }
+
+//     int i,min;
+//     for(i=0;i<s;i++)
+//     {
+//         min=i;
+//         for(int j=i+1;j<s;j++)
+//         {
+//             if(arr[min]>arr[j])
+//             {
+//                 min=j;
+//             }
+//         }
+//         if(min!=i)
+//         {
+//             int swp;
+//             swp=arr[min];
+//             arr[min]=arr[i];
+//             arr[i]=swp;
+//         }
+//     }
+//     cout<<"\n after selection sort\n";
+//     for(int i=0;i<s;i++)
+//     {
+//         cout<<arr[i]<<"\t";
+//     }
+// }
+
+#include<iostream> 
 using namespace std;
-int main()
-{
-    int arr[]={3,14,2,5,7};
-    int s=sizeof(arr)/sizeof(arr[0]);
-
-    for(int i=0;i<s;i++)
-    {
+void Selection(int arr[], int s){
+      int c=0;
+    for(int i=0;i<s;i++){
+          int min=i;
+        for(int j=i+1;j<s;j++){
+             c++;  
+            if(arr[j]<arr[min]){
+                min=j;
+              }
+        }
+        if(min!=i){
+            int swp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=swp;
+        }
+    }
+    cout<<"Number of hits :"<<c;
+}
+int main(){
+    int s;
+    int arr[]={9,4,3,9,1};
+    s=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<s;i++){
         cout<<arr[i]<<"\t";
     }
-
-    int i,min;
-    for(i=0;i<s;i++)
-    {
-        min=i;
-        for(int j=i+1;j<s;j++)
-        {
-            if(arr[min]>arr[j])
-            {
-                min=j;
-            }
-        }
-        if(min!=i)
-        {
-            int swp;
-            swp=arr[min];
-            arr[min]=arr[i];
-            arr[i]=swp;
-        }
-    }
-    cout<<"\n after selection sort\n";
-    for(int i=0;i<s;i++)
-    {
+    Selection(arr, s);
+    cout<<"\nAfter swaping\n";
+    for(int i=0;i<s;i++){
         cout<<arr[i]<<"\t";
     }
 }
+
+// Time complexity in all cases is O(n)
+// It is inplace 
+// Space Complexity   O(1)
+// Selection sort is not stable
+// OPtimisation  is not possible
+// Approach Subraction and Conquer 
