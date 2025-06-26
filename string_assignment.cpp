3416429627f4af36eace6d22e4d824e9ba05c31b
harsh14thakre <harsh14thakre@gmail.com>
2025-06-26 17:02:14 +0530

    commit

---
+++ string_assignment.cpp
@@ -0,0 +1,97 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string a;
+//     cout<<"Enter name\n";
+//     getline(cin,a);
+//     cout<<"Your name="<<a;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     string a(5,'j');
+//     cout<<a<<"\n";
+//     string j="415935";
+//     // sort(j.begin(),j.end(),less<int>());
+//     // cout<<j;
+//     sort(j.begin(),j.end(),greater<int>());
+//     cout<<j;
+
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     string a(5,'j');
+//     cout<<a<<"\n";
+//     string j="41595572";
+//     // sort(j.begin(),j.end(),less<int>());
+//     // cout<<j;
+//     cout<<j.size();
+//     j.resize(6);
+//     cout<<"j="<<j;
+
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     string s="cybrombhopal";
+//     cout<<"\n";
+//     cout<<s.find("rom");
+//     cout<<s.back();
+//     cout<<s.front();
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     string j="41595572";
+//     // string s="cybrombhopal";
+//     // cout<<"\n";
+//     // cout<<s.find("rom");
+//     // cout<<s.back();
+//     // cout<<s.front();
+//     // cout<<j+s;  //Concatinate
+//     // cout<<"\n";
+//     // j.append(s);  //Both j and s will merge together
+//     // cout<<"j="<<j;
+
+//     j.insert(2,"hello");
+//     cout<<"new j="<<j;
+// }
+
+
+
+
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<string>
+int main()
+{
+    string h="mpnagar";
+    cout<<"h="<<h<<"\n";
+    h.erase(2,3);
+    cout<<"\n after erase value of h="<<<h<<endl;
+    // h.clear():
+}
