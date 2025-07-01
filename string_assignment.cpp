bf5bc1fc08a6b1b052e7365796e6621d07a69dd9
harsh14thakre <harsh14thakre@gmail.com>
2025-07-01 16:34:33 +0530

    commit

--- string_assignment.cpp
+++ string_assignment.cpp
@@ -83,15 +83,117 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     string h="mpnagar";
+//     cout<<"h="<<h<<"\n";
+//     h.erase(2,3);
+//     cout<<"\n after erase value of h="<<<h<<endl;
+//     // h.clear():
+// }
+
+
+
+// Q1. Reverse a given string
+
+/// #include<iostream>
+// using namespace std;
+// #include<string>
+// #include<algorithm>
+
+// int main(){
+
+//   string s="Cybrom";
+//   reverse(s.begin(),s.end());
+//   cout<<s;
+// }
+
+
+// Q2. Replace Each Letter with the next in the Alphabet
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     s="cybrom";
+
+// }
+
+
+// Q3. Capitalize the first letter of each word
+
+// #include<iostream>
+// using namespace std;
+// #include<algorithm>
+// #include<string>
+// int main()
+// {
+//     s="cpp string exercises";
+
+
+// }
+
+
+// Q4. Find the largest word in the string
+// input=Cpp is a general-purpose programming language.
+// output=programming
+
+// #include<iostrem>
+// using namespace std;
+// int main()
+// {
+//     char s1[10]="listen";
+//     char s2[10]="silents";
+//     int c[26]={0};
+// }
+
+
+
+
+
+
+
+// Write a program to find the missing numbers;
+// Remove duplicate values using vector.
+\
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     vector<int>v{4,1,3,2,5};
+//     sort(v.begin(),v.end(),less;<int>());
+//     for(auto p:v)
+//     {
+//         cout<<p<<"\n";
+//     }
+// }
+
+
+
 #include<iostream>
 using namespace std;
-#include<algorithm>
-#include<string>
 int main()
 {
-    string h="mpnagar";
-    cout<<"h="<<h<<"\n";
-    h.erase(2,3);
-    cout<<"\n after erase value of h="<<<h<<endl;
-    // h.clear():
+    vector<int>v{4,1,3,2,5};
+   v.insert (v.begin()-1,10)
 }
+
+
+
+
+
+
